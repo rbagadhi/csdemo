aa20ae1c1983e968dbce3e39b89adab3176692aa
Raghavendra Bagadhi <raghavendra.bagadhi@gmail.com>
2017-12-18 22:03:29 +0530

    Update deploy.bat

--- scripts/deploy.bat
+++ scripts/deploy.bat
@@ -3,7 +3,7 @@ if "%1"=="" (
   echo Please provide the path to the application archive.
 ) else (
   echo Creating Config Server...
-  cf create-service -c '{ "git": { "uri": "https://github.com/rbagadhi/pcf-scs-config", "label": "master"  } }' p-config-server standard config-server > nul
+  cf create-service -c '{ "git": { "uri": "https://github.com/rbagadhi/config", "label": "master"  } }' p-config-server standard config-server > nul
   :check
     cf service config-server | find "succeeded" > nul
     if errorlevel 1 goto :check
